bc67dfb0f91a0a17f8290f97e6e3c920a445ef19
Shivo <shivo@shivo-Presario-CQ56-Notebook-PC.(none)>
2013-09-11 13:08:07 +0300

    text

---
+++ dolor_sit.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Dolor Sit</title>
+		<meta charset="utf-8">
+		<style type="text/css">
+			body {
+				font-family: Tahoma;
+			}
+			a {
+				text-decoration: none;
+			}
+			h1 {
+   				font-weight: 200;
+   				font-size: 46px;
+   			}
+			hr {
+				border: none;
+    			color: #E6E6FA;
+   				background-color: #E6E6FA;
+   				height: 1px;
+   			}
+			.container {
+				margin-left: auto;
+				margin-right: auto;
+				width: 800px;
+			}
+			.menu {
+				float: right;
+				margin-top: 25px;
+			}
+			.content {
+				width: 100%;
+				margin-top: 10px;
+			}
+			
+			.mr {
+				margin-right: 30px;
+			}
+		</style>
+	</head>
+	<body>
+		<br>
+		<div class="container">
+			<div class="menu">
+				<a href="index.php" class="mr">Hello world!</a>
+				<a href="Lorem_ipsum.php" class="mr">Lorem ipsum</a>
+				<a href="Amet.php">Amet</a>
+				
+			</div>
+			<div class="content">
+				<h1>Dolor sit</h1>
+				<hr>
+				<p>
+					Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+				</p>
+			</div>
+		</div>
+	</body>
+</html>
